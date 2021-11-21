df426c5676c2205e201e00a75d0a27e924f5625a
Wilbertson <w.kreativo@gmail.com>
2021-11-21 07:16:36 -0500

    decorador_api_view

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -2,13 +2,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from apps.users.api.serializers import UserSerializer
+from rest_framework.decorators import api_view
 
 User = get_user_model()
 
 
-class UserAPIView(APIView):
+@api_view(['GET'])
+def user_api_view(request):
 
-    def get(self, request):
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializers = UserSerializer(users, many=True)
         return Response(users_serializers.data)
